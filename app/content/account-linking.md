cd14b635f292154c820940b568e2fc3bd17d3378
Erin Sparling <sparling@google.com>
2024-01-08 21:59:48 +0000

    Draft of a stub entitlements endpoint

---
+++ app/content/account-linking.md
@@ -0,0 +1,10 @@
+<script async
+  subscriptions-control="manual" 
+  type="application/javascript"
+  src="https://news.google.com/swg/js/v1/swg.js">
+</script>
+
+# Account Linking
+## Client-side Javascript Demo
+
+This form has a randomly created ppid, but can be set by the reader.
--- server.js
+++ server.js
@@ -23,6 +23,7 @@ import readme from './app/routes/readme.js';
 import subscriptionLinkingApi from './app/routes/subscription-linking/api.js';
 import publicationApi from './app/routes/publication-api.js';
 import pubSub from './app/routes/pub-sub.js';
+import accountLinkingApi from './app/routes/account-linking/api.js';
 
 // Proxy handles https and reverse proxy settings for running locally
 import proxy from './middleware/proxy.js';
@@ -65,6 +66,7 @@ app.get('/css/*', async (req, res)=>{
 app.use('/api/subscription-linking', subscriptionLinkingApi);
 app.use('/api/publication', publicationApi);
 app.use('/api/pub-sub', pubSub);
+app.use('/api/account-linking', accountLinkingApi);
 
 // Boot the server
 console.log(
