e01cb99c8104d5ae6029d13b49950ea531270091
Erin Sparling <sparling@google.com>
2024-07-22 16:54:55 +0000

    Added draft of survey implementation

---
+++ app/content/survey-manual.md
@@ -0,0 +1,15 @@
+<script async
+  subscriptions-control="manual"
+  type="application/javascript"
+  src="https://news.google.com/swg/js/v1/swg-qual.js">
+</script>
+
+
+# Survey prompts
+
+This page both demonstrates how RRM:E survey sign-up works, as well as documents
+how to implement the prompts.
+
+## Test the manual survey invocation
+
+<div id="newsletterPrompts"></div>
---
+++ public/js/survey-manual.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview This client-side js file to handle newsletter prompts
+ */
+
+import {
+  createButtonForPrompt,
+  registerEventManager,
+} from './newsletter-methods.js';
+
+const newsletterConfigurations = [
+  {
+    name: 'Very detailed survey',
+    configurationId: '14965053-83a6-4efa-98e4-223450cd5801',
+  },
+  {
+    name: 'Benefits survey',
+    configurationId: 'bd36617e-595e-488f-85e5-7cbde1ce1978',
+  }
+];
+
+const buttonContainer = document.querySelector('#newsletterPrompts');
+
+(self.SWG = self.SWG || []).push(async (subscriptions) => {
+  subscriptions.configure({paySwgVersion: '2'});
+  subscriptions.init('process.env.PUBLICATION_ID');
+
+  await registerEventManager(subscriptions);
+
+  const availableInterventions =
+    await subscriptions.getAvailableInterventions();
+
+  console.log({availableInterventions});
+
+  const availableInterventionConfigurationIds = availableInterventions.map(
+    (intervention) => intervention.configurationId
+  );
+
+  for (const newsletterConfiguration of newsletterConfigurations) {
+    const buttonEnabledState = availableInterventionConfigurationIds.includes(
+      newsletterConfiguration.configurationId
+    );
+
+    createButtonForPrompt(
+      availableInterventions,
+      newsletterConfiguration,
+      buttonEnabledState,
+      buttonContainer
+    );
+  }
+});
