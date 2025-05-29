40b2c85f60b7bd7316ca1a6524cb9e87efeccd81
Akira Sakaguchi <akirasakaguchi@google.com>
2025-05-29 07:34:46 +0000

    wip - sl bundle demo

--- app/content/subscription-linking.md
+++ app/content/subscription-linking.md
@@ -1,16 +1,38 @@
 <script async
   subscriptions-control="manual" 
   type="application/javascript"
-  src="https://news.google.com/swg/js/v1/swg.js">
+  src="https://news.google.com/swg/js/v1/swg-qual.js">
 </script>
 
 # Subscription Linking
+
 ## Client-side Javascript Demo
 
 This form has a randomly created ppid, but can be set by the reader.
 
 ### Initiate linking with random ppid {#initiateLink}
 
+<br>
+
+```javascript
+subscriptions.linkSubscriptions({publisherProvidedId: `ppid` });
+```
+
+### Bundle multiple publications for Subscripton Linking {#bundleLink}
+
+You can bundle multiple publications for Subscription Linking at once.
+
+```javascript
+subscriptions.linkSubscriptions({linkTo: [
+  { publicationId: 'pubId1', publisherProvidedId: 'ppid1' },
+  { publicationId: 'pubId2', publisherProvidedId: 'ppid2' },
+   …
+]});
+```
+
+<button id="bundleSlButton">Bundle Subscription Linking</button>
+
+## Server-side API call demo
 
 ### Query Entitlements with specified ppid {#queryPpid}
 
--- public/js/subscription-linking.js
+++ public/js/subscription-linking.js
@@ -22,7 +22,7 @@ import { AnalyticsEventHandler } from './subscription-linking-event-handler.js';
 /**
  * linkSubscription
  * Initiates a linked subscription
- * @param {number} ppid
+ * @param {number} {ppid}
  */
 function linkSubscription(ppid, eventHandler) {
   self.SWG.push(async (subscriptions) => {
@@ -38,6 +38,25 @@ function linkSubscription(ppid, eventHandler) {
 }
 
 /**
+ * linkSubscriptions: link multiple publications
+ * @param {array} 
+ */
+function linkSubscriptions(slDataArr, eventHandler) {
+  console.log('linkSubscriptions ', slDataArr);
+  self.SWG.push(async (subscriptions) => {
+      try {
+        const result = await subscriptions.linkSubscriptions({linkTo: [
+            { publicationId: slDataArr[0].publicationId, publisherProvidedId: slDataArr[0].ppid },
+            { publicationId: slDataArr[1].publicationId, publisherProvidedId: slDataArr[1].ppid }
+        ]});
+        console.log('bundle result - ', result);
+        eventHandler.logCallback(result);
+      } catch(e) {
+        console.log(e);
+      }
+  });
+}
+/**
  * randomPpid()
  * Generates a random ppid
  * @returns {string}
@@ -48,10 +67,10 @@ function randomPpid() {
 
 
 /**
- * createForm()
+ * createSingleSLForm()
  * Creates a form, and appends it to the DOM after #initiateLink
  */
-function createForm(eventHandler) {
+function createSingleSLForm(eventHandler) {
   let ppid = randomPpid();
 
   const button = document.createElement('button');
@@ -78,12 +97,100 @@ function createForm(eventHandler) {
 }
 
 /**
+ * createBundleSLForm()
+ * Creates a form, and appends it to the DOM after #initiateLink
+ */
+function createBundleSLForm(eventHandler) {
+  let ppid1 = randomPpid();
+  let ppid2 = randomPpid();
+  let pubId1 = 'CAow-tCJAQ';
+  let pubId2 = 'CAow-9CJAQ';
+
+  const button = document.createElement('button');
+  button.innerText = "Bundle Multiple Subscription Linking";
+  button.addEventListener('click', (event)=>{
+    event.preventDefault();
+    const slDataArr = [
+      {'publicationId':pubId1, 'ppid': ppid1},
+      {'publicationId':pubId2, 'ppid': ppid2},
+    ];
+    linkSubscriptions(slDataArr, eventHandler);
+  });
+
+  const inputPPID1 = document.createElement('input');
+  inputPPID1.setAttribute('value', ppid1);
+  inputPPID1.setAttribute('id', 'bundle-ppid-input-1');
+  inputPPID1.onchange = (event)=>{
+    ppid1 = event.target.value;
+  };
+
+  const inputPPID2 = document.createElement('input');
+  inputPPID2.setAttribute('value', ppid2);
+  inputPPID2.setAttribute('id', 'bundle-ppid-input-2');
+  inputPPID2.onchange = (event)=>{
+    ppid2 = event.target.value;
+  };
+
+  const inputPubId1 = document.createElement('input');
+  inputPubId1.setAttribute('value', pubId1);
+  inputPubId1.setAttribute('id', 'bundle-pubid-input-1');
+  inputPubId1.onchange = (event)=>{
+    pubId1 = event.target.value;
+  };
+
+  const inputPubId2 = document.createElement('input');
+  inputPubId2.setAttribute('value', pubId2);
+  inputPubId2.setAttribute('id', 'bundle-pubid-input-2');
+  inputPubId2.onchange = (event)=>{
+    pubId2 = event.target.value;
+  };  
+
+   // Create containers for each row
+  const row1 = document.createElement('div');
+  row1.style.display = 'flex'; // Use flexbox for horizontal alignment
+  row1.style.gap = '10px'; // Add some space between inputs
+  row1.style.marginBottom = '10px'; // Add space below the row
+  row1.appendChild(inputPubId1);
+  row1.appendChild(inputPPID1);
+
+  const row2 = document.createElement('div');
+  row2.style.display = 'flex'; // Use flexbox for horizontal alignment
+  row2.style.gap = '10px'; // Add some space between inputs
+  row2.style.marginBottom = '10px'; // Add space below the row
+  row2.appendChild(inputPubId2);
+  row2.appendChild(inputPPID2);
+
+    // Create the header row
+  const headerRow = document.createElement('div');
+  headerRow.style.display = 'flex';
+  headerRow.style.gap = '80px';
+  headerRow.style.fontWeight = 'bold'; // Make the headers bold
+  headerRow.style.marginBottom = '5px'; // Small space below headers
+
+  const ppidHeader = document.createElement('div');
+  ppidHeader.innerText = 'PPID';
+  const pubIdHeader = document.createElement('div');
+  pubIdHeader.innerText = 'Publication ID';
+  headerRow.appendChild(pubIdHeader);
+  headerRow.appendChild(ppidHeader);
+
+  const form = document.createElement('form');
+  form.appendChild(headerRow); 
+  form.appendChild(row1);
+  form.appendChild(row2);
+  form.appendChild(button);
+
+  document
+      .querySelector('#bundleLink')
+      .insertAdjacentElement('afterend', form);
+}
+
+/**
  * createQueryForm()
  * Creates a form, and appends it to the DOM after #initiateLink.
  * Used for querying entitlements from the api
  */
 function createQueryForm() {
-
   let ppid = '';
 
   const output = document.createElement('pre');
@@ -120,6 +227,7 @@ function createQueryForm() {
 
   document.querySelector('#queryPpid').insertAdjacentElement('afterend', form);
 }
+
 
 
 /**
@@ -224,7 +332,8 @@ function analyticsEventLogger(subs, eventHandler) {
 document.addEventListener('DOMContentLoaded', function () {
   const eventHandler = new AnalyticsEventHandler();
 
-  createForm(eventHandler);
+  createSingleSLForm(eventHandler);
+  createBundleSLForm(eventHandler);
   createQueryForm();
   createUpdateForm();
   (self.SWG = self.SWG || []).push(subscriptions => {
